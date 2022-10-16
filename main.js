acd089ac83cbc36764117cd4af24b1092987fd1b
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-10-16 14:47:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("This is a message from Snehasis - 'We all are human'");
+song1 = "";
+song2 = "";
+function preload(){
+ song1 = loadSound("music.mp3");
+ song2 = loadSound("music2.mp3");
+}
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+    image(video,0,0,600,500)
+}
